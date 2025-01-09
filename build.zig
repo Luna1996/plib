d8b4644c1ac173941e92851d0f5dcc260d8fca86
四光年 <wyf19960926@gmail.com>
2025-01-09 20:36:54 +0800

    restructure

--- build.zig
+++ build.zig
@@ -3,21 +3,20 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
   const target = b.standardTargetOptions(.{});
   const optimize = b.standardOptimizeOption(.{});
-  const file = b.option([]const u8, "file", "The file to be built.") orelse {
-    std.log.err("Require a .zig file to be built.", .{});
-    return;
-  };
-  const base = std.fs.path.basename(file);
-  const opts = .{
-    .name = base[0..base.len - 4],
-    .root_source_file = .{ .path = file },
+  
+  const root_mod = b.addModule("root", .{
     .target = target,
     .optimize = optimize,
-    .main_pkg_path = std.Build.LazyPath.relative("src"),
-  };
+    .root_source_file = b.path("src/root.zig"),
+  });
 
-  b.getInstallStep().dependOn(&b.addInstallArtifact(b.addExecutable(opts), .{}).step);
-  
-  b.step("test", "build test binary for current module")
-    .dependOn(&b.addInstallArtifact(b.addTest(opts), .{}).step);
+  const test_exe = b.addTest(.{
+    .target = target,
+    .optimize = optimize,
+    .root_module = root_mod,
+  });
+  const test_ins = b.addInstallArtifact(test_exe, .{});
+
+  const test_step = b.step("test", "test library");
+  test_step.dependOn(&test_ins.step);
 }
---
+++ src/root.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+test "main" {
+  std.debug.print("YES\n", .{});
+}
