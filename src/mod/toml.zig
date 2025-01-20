d015a0aa673b0ff3b688c761af60ff1ff8c9fa93
YifuWang <wyf19960926@gmail.com>
2025-01-20 16:57:56 +0800

    [WIP] toml builder

--- src/mod/toml.zig
+++ src/mod/toml.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+
+const zeit = @import("zeit");
+
 const plib = @import("plib");
 const Parser = plib.Parser(@import("gen").abnf);
 const Tag = Parser.Tag;
@@ -6,21 +9,25 @@ const Node = Parser.Node;
 const Array = std.ArrayList;
 const Table = std.StringHashMap;
 
+const unescape = @import("toml/string.zig").unescape;
+
 pub const Toml = struct {
+  allocator: std.mem.Allocator,
+  root: Value,
+
+  const Self = @This();
+
   pub const Value = union(enum) {
     string: []const u8,
     boolean: bool,
     array: Array(Value),
     table: Table(Value),
-    date_time: usize,
+    nanosec: zeit.Nanoseconds,
     float: f64,
     integer: i64,
   };
 
-  allocator: std.mem.Allocator,
-  root: Value,
-
-  const Self = @This();
+  pub const ValueType = std.meta.Tag(Value);
 
   const Conf = struct {
     allocator: std.mem.Allocator,
@@ -32,11 +39,13 @@ pub const Toml = struct {
     const root = try parse(conf);
     defer root.deinit();
 
-    const self = Self {
+    var self = Self {
       .allocator = conf.allocator,
       .root = Table(Value).init(conf.allocator),
     };
     errdefer self.deinit();
+
+    return self;
   }
 
   pub fn parse(conf: Conf) !Node {
@@ -45,7 +54,10 @@ pub const Toml = struct {
       .input = conf.input,
       .keeps = &.{
         .toml, .key, .std_table, .array_table,
-        .string, .boolean, .array, .inline_table, .date_time, .float, .integer,
+        .unquoted_key, .basic_string, .literal_string,
+        .ml_basic_string, .ml_literal_string,
+        .boolean, .array, .inline_table, .float, .integer,
+        .offset_date_time, .local_date_time, .local_date, .local_time,
       },
       .file_path = conf.file_path,
     });
@@ -63,7 +75,19 @@ pub const Toml = struct {
     self.deinitValue(self.root);
   }
 
-  fn deinitValue(self: Self, value: Value) void {
+  pub fn initValue(self: Self, wht: ValueType) Value {
+    return switch (wht) {
+      .string => .{.string = undefined},
+      .boolean => .{.boolean = undefined},
+      .array => .{.array = Array(Value).init(self.allocator)},
+      .table => .{.table = Table(Value).init(self.allocator)},
+      .nanosec => .{.nanosec = undefined},
+      .float => .{.float = undefined},
+      .integer => .{.integer = undefined},
+    };
+  }
+
+  pub fn deinitValue(self: Self, value: Value) void {
     switch (value) {
       .array => |array| {
         for (array.items) |item| self.deinitValue(item);
@@ -77,11 +101,92 @@ pub const Toml = struct {
         }
         table.deinit();
       },
+      .string => |string| {
+        self.allocator.free(string);
+      },
       else => {},
     }
   }
 
-  // fn buildToml(self: Self, root: Node) !void {}
+  fn buildToml(self: *Self, root: Node) !void {
+    const items = root.val.sub.items;
+    var i: usize = 0;
+    var ctx = self.root;
+    while (i < items.len) {
+      const item = items[i];
+      switch (item.tag.?) {
+        .key => {
+          i += 1;
+          const next = items[i];
+          if (self.resolveMulByNode(&ctx, item, nodeTagToValueType(next.tag.?))) {
+            return error.TomlError;
+          }
+          // TODO
+        },
+        .std_table => {},
+        .array_table => {},
+        else => unreachable,
+      }
+      i += 1;
+    }
+  }
+
+  fn resolveMulByNode(self: Self, ctx: *Value, key: Node, wht: ValueType) !bool {
+    const sub_len = key.subLen();
+    for (key.val.sub.items[0..sub_len - 1]) |sub|
+      _ = try self.resolveOneByNode(ctx, sub, .table);
+    return try self.resolveOneByNode(ctx, key.val.sub.items[sub_len - 1], wht);
+  }
+
+  fn resolveOneByNode(self: Self, ctx: *Value, key: Node, wht: ValueType) !bool {
+    const str = switch (key.tag.?) {
+      .unquoted_key => key.val.str,
+      .basic_string => try unescape(self.allocator, key.val.str[1..key.val.str.len - 1]),
+      .literal_string => key.val.str[1..key.val.str.len - 1],
+      else => unreachable,
+    };
+   
+    if (ctx.table.get(str)) |sub| {
+      if (key.tag.? == .basic_string)
+        self.allocator.free(str);
+      if (std.meta.activeTag(sub) != wht)
+        return error.TomlError;
+      ctx.* = sub;
+      return true;
+    }
+
+    const sub_key = switch (key.tab.?) {
+      .basic_string => str,
+      .unquoted_key, .literal_string => try self.allocator.dupe(u8, str),
+      else => unreachable,
+    };
+    errdefer self.allocator.free(sub_key);
+
+    const sub = self.initValue(wht);
+    try ctx.table.put(sub_key, sub);
+    ctx.* = sub;
+
+    return false;
+  }
+
+  fn nodeTagToValueType(tag: Tag) ValueType {
+    return switch (tag) {
+      .basic_string,
+      .literal_string,
+      .ml_basic_string,
+      .ml_literal_string => .string,
+      .boolean           => .boolean,
+      .array             => .array,
+      .inline_table      => .table,
+      .float             => .float,
+      .integer           => .integer,
+      .offset_date_time,
+      .local_date_time,
+      .local_date,
+      .local_time        => .nanosec,
+      else               => unreachable,
+    };
+  }
 };
 
 test "toml" {
---
+++ src/mod/toml/string.zig
@@ -0,0 +1,3 @@
+const std = @import("std");
+
+pub fn unescape(allocator: std.mem.Allocator, text: []const u8) ![]u8 {}
