3f72f8645ed79f4398097fdccd8d49c8ab0624c0
四光年 <wyf19960926@gmail.com>
2025-01-23 21:37:56 +0800

    [WIP] reconstruct builder

--- src/mod/toml/build_toml.zig
+++ src/mod/toml/build_toml.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
-const Self = @import("root.zig").Toml;
+const Toml = @import("root.zig").Toml;
 const esc = @import("escape.zig");
-const Tag = Self.Tag;
-const Ast = Self.Ast;
-const DateTime = Self.DateTime;
+const Tag = Toml.Tag;
+const Ast = Toml.Ast;
+const DateTime = Toml.DateTime;
+
+const asTag = std.meta.activeTag;
 
 const BuildError = error {TomlError, DateTimeError} 
   || std.mem.Allocator.Error
@@ -11,11 +13,44 @@ const BuildError = error {TomlError, DateTimeError}
   || std.fmt.ParseFloatError
   || error { Utf8CannotEncodeSurrogateHalf, CodepointTooLarge };
 
-const State = enum(u1) {explicit, implicit};
-const Visits = std.AutoHashMapUnmanaged(*Self, State);
+const Closed = std.AutoHashMapUnmanaged(*Toml, void);
 
-pub fn buildToml(allocator: std.mem.Allocator, ast: *const Ast) !Self {
-  var root = Self.init(.table);
+pub const TomlBuilder = struct {
+  allocator: std.mem.Allocator,
+  closed: Closed = Closed.empty,
+  root: Toml,
+  current_table: *Toml,
+
+  const Self = @This();
+
+  pub fn build(allocator: std.mem.Allocator, ast: *const Ast) !Toml {
+    var self = Self {
+      .allocator = allocator,
+      .root = Toml.init(.table),
+    };
+    defer self.deinit();
+    errdefer self.root.deinit(allocator);
+
+    switch (ast.val) {
+      .str => {},
+      .sub => |sub| for (sub.items, 1..) |*item, i| switch (item.tag.?) {
+        .key => {},
+        .std_table => {},
+        .array_table => {},
+        else => {},
+      },
+    }
+
+    return self.root;
+  }
+
+  fn deinit(self: *Self) void {
+    self.closed.deinit(self.allocator);
+  }
+};
+
+pub fn buildToml(allocator: std.mem.Allocator, ast: *const Ast) !Toml {
+  var root = Toml.init(.table);
   errdefer root.deinit(allocator);
 
   var visits = Visits.empty;
@@ -51,7 +86,7 @@ pub fn buildToml(allocator: std.mem.Allocator, ast: *const Ast) !Self {
 }
 
 fn buildKeyVal(
-  self: *Self,
+  self: *Toml,
   allocator: std.mem.Allocator,
   key: *const Ast,
   val: *const Ast,
@@ -65,7 +100,7 @@ fn buildVal(
   allocator: std.mem.Allocator,
   ast: *const Ast,
   visits: *Visits,
-) BuildError!Self {
+) BuildError!Toml {
   return switch (ast.tag.?) {
     .basic_string,
     .literal_string,
@@ -84,29 +119,29 @@ fn buildVal(
   };
 }
 
-fn buildString(allocator: std.mem.Allocator, ast: *const Ast) !Self {
+fn buildString(allocator: std.mem.Allocator, ast: *const Ast) !Toml {
   const str, const need_free = try esc.unescape(allocator, ast);
   return .{.string = if (need_free) str else try allocator.dupe(u8, str)};
 }
 
-fn buildInteger(ast: *const Ast) !Self {
+fn buildInteger(ast: *const Ast) !Toml {
   return .{.integer = try std.fmt.parseInt(i64, ast.val.str, 0)};
 }
 
-fn buildFloat(ast: *const Ast) !Self {
+fn buildFloat(ast: *const Ast) !Toml {
   return .{.float = try std.fmt.parseFloat(f64, ast.val.str)};
 }
 
-fn buildBoolean(ast: *const Ast) Self {
+fn buildBoolean(ast: *const Ast) Toml {
   return .{.boolean = std.mem.eql(u8, ast.val.str, "true")};
 }
 
-fn buildDateTime(ast: *const Ast) !Self {
+fn buildDateTime(ast: *const Ast) !Toml {
   return .{.datetime = try DateTime.fromRFC3339(ast.val.str)};
 }
 
-fn buildArray(allocator: std.mem.Allocator, ast: *const Ast, visits: *Visits) !Self {
-  var self = Self.init(.array);
+fn buildArray(allocator: std.mem.Allocator, ast: *const Ast, visits: *Visits) !Toml {
+  var self = Toml.init(.array);
   errdefer self.deinit(allocator);
   if (std.meta.activeTag(ast.val) == .str) return self;
   for (ast.val.sub.items) |*item| {
@@ -117,8 +152,8 @@ fn buildArray(allocator: std.mem.Allocator, ast: *const Ast, visits: *Visits) !S
   return self;
 }
 
-fn buildTable(allocator: std.mem.Allocator, ast: *const Ast, visits: *Visits) !Self {
-  var self = Self.init(.table);
+fn buildTable(allocator: std.mem.Allocator, ast: *const Ast, visits: *Visits) !Toml {
+  var self = Toml.init(.table);
   errdefer self.deinit(allocator);
   if (std.meta.activeTag(ast.val) == .str) return self;
   var i: usize = 0;
@@ -130,34 +165,34 @@ fn buildTable(allocator: std.mem.Allocator, ast: *const Ast, visits: *Visits) !S
 }
 
 fn resolveTable(
-  self: *Self,
+  self: *Toml,
   allocator: std.mem.Allocator,
   ast: *const Ast,
   visits: *Visits,
-) !*Self {
+) !*Toml {
   return try resolveMulByAst(self, allocator, ast.get(0), visits, .explicit, .table);
 }
 
 fn resolveArray(
-  self: *Self,
+  self: *Toml,
   allocator: std.mem.Allocator,
   ast: *const Ast,
   visits: *Visits,
-) !*Self {
+) !*Toml {
   const next = try resolveMulByAst(self, allocator, ast.get(0), visits, .implicit, .array);
   const item = try next.array.addOne(allocator);
-  item.* = Self.init(.table);
+  item.* = Toml.init(.table);
   return item;
 }
 
 fn resolveMulByAst(
-  self: *Self,
+  self: *Toml,
   allocator: std.mem.Allocator,
   ast: *const Ast,
   visits: *Visits,
   comptime state: State,
   comptime expect: Tag,
-) !*Self {
+) !*Toml {
   const keys = ast.val.sub.items;
   const len = keys.len;
   var current_table = self;
@@ -171,13 +206,13 @@ fn resolveMulByAst(
 }
 
 fn resolveOneByAst(
-  self: *Self,
+  self: *Toml,
   allocator: std.mem.Allocator,
   ast: *const Ast,
   visits: *Visits,
   comptime state: State,
   comptime expect: Tag,
-) !*Self {
+) !*Toml {
   var key, const need_free = try esc.unescape(allocator, ast);
   if (self.table.getPtr(key)) |val| {
     if (need_free) allocator.free(key);
@@ -196,6 +231,6 @@ fn resolveOneByAst(
   if (!need_free) key = try allocator.dupe(u8, key);
   errdefer allocator.free(key);
   const res = try self.table.getOrPut(allocator, key);
-  res.value_ptr.* = Self.init(expect);
+  res.value_ptr.* = Toml.init(expect);
   return res.value_ptr;
 }
