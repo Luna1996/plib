b7e60ec270998e0ead7c677ff67fb056c08e856a
YifuWang <wyf19960926@gmail.com>
2025-02-26 16:43:26 +0800

    formatter_full.zig

--- src/mod/toml/formatter_full.zig
+++ src/mod/toml/formatter_full.zig
@@ -10,6 +10,8 @@ allocator: std.mem.Allocator,
 ast: Ast = Ast.initStr(""),
 val: Toml = Toml.init(.table),
 
+current_table: *Toml = undefined,
+
 last_expression_exist: bool = true,
 next_expression_comment: []const u8 = "",
 skip_until_next_table: bool = false,
@@ -33,28 +35,29 @@ pub fn format(
   _: std.fmt.FormatOptions,
   writer: anytype,
 ) !void {
+  self.current_table = &self.val;
   switch (self.ast.val) {
     .str => |str| if (str.len != 0) {
       try writer.writeAll(str);
       if (str[str.len - 1] != '\n') try writer.writeByte('\n');
     },
-    .sub => |sub| for (sub.items) |ast| if (ast.tag) |tag| switch (tag) {
+    .sub => |sub| for (sub.items, 0..) |ast, i| if (ast.tag) |tag| switch (tag) {
       .keyval => try self.formatKeyVal(ast, writer),
       else => try self.formatSectionHead(ast, writer),
-    } else self.formatFiller(ast, writer),
+    } else self.formatFiller(ast.val.str, i == 0, writer),
   }
   try self.formatRemain(true, writer);
 }
 
-fn formatFiller(self: *Self, ast: Ast, writer: anytype) !void {
+fn formatFiller(self: *Self, filler: []const u8, is_start: bool, writer: anytype) !void {
   if (self.skip_until_next_table) return;
-  const res = splitNull(ast.val.str);
+  const res = splitFiller(filler, is_start);
   if (self.last_expression_exist) try writer.writeAll(res[0]);
   try writer.writeAll(res[1]);
   self.next_expression_comment = res[2];
 }
 
-fn splitNull(str: []const u8) [3][]const u8 {}
+fn splitFiller(filler: []const u8, is_start: bool) [3][]const u8 {}
 
 fn formatKeyVal(self: *Self, ast: Ast, writer: anytype) !void {}
 
